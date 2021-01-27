8f435f3ce207ea94f58be3ff8db7bc09275466c7
RobertGraham-Hu <robertgrahamhu@gmail.com>
2021-01-27 10:21:15 -0500

    Fixed dashboard map rendering

--- src/views/Dashboard/components/dashboard.tsx
+++ src/views/Dashboard/components/dashboard.tsx
@@ -1,29 +1,45 @@
-import { useContext } from "react";
+import { useContext, useEffect, useRef } from "react";
 import { useHistory } from "react-router-dom";
 import firebase from "../../../firebase";
 import { AuthContext } from "../../../AuthProvider";
 import WebMap from "@arcgis/core/WebMap";
-import MapView from "@arcgis/core/views/MapView"
+import MapView from "@arcgis/core/views/MapView";
+import Map from "@arcgis/core/Map";
+import TileLayer from "@arcgis/core/layers/TileLayer";
 
 import "firebase/firestore";
 
 const Dashboard = () => {
+    const arcViewRef = useRef<HTMLDivElement>(null);
     const { logout } = useContext(AuthContext);
-
-
     const handleClick = (event: any) => {
         event.preventDefault();
         logout();
     }
 
-    const webmap = new WebMap({
-        portalItem: { // autocasts as new PortalItem()
-          id: "2d3d8f71b9ee47afa34b4ac377b41b19"
-    }});
+    useEffect(() => {
+        if (arcViewRef.current) {
+            const map = new Map({
+                basemap: "topo-vector"
+              });
+        
+            const view = new MapView({
+                container: arcViewRef.current,
+                map: map,
+                zoom: 14,
+                center: [-80.58, 43.48]
+            });
 
-    const view = new MapView({
-        map: webmap,
-        container: "arcViewDiv"
+            var larm = new TileLayer({
+                url: "https://tiles.arcgis.com/tiles/wlGPBvwc5LdaCZBr/arcgis/rest/services/ArcOnline_jan1/MapServer"
+              });
+            map.add(larm);
+            
+            var larmCosts = new TileLayer({
+                url: "https://tiles.arcgis.com/tiles/wlGPBvwc5LdaCZBr/arcgis/rest/services/CM1_Gnarly_costs/MapServer"
+            });
+            map.add(larmCosts);
+        }
     });
 
     return (
@@ -31,7 +47,7 @@ const Dashboard = () => {
             <h1>Dashboard</h1>
             <h2>Welcome to Dashboard!</h2>
             <button onClick={handleClick}>Logout</button>
-            <div className="arcViewDiv" style={{ width: '100vw', height: '100vh' }}>
+            <div ref={arcViewRef} className="arcViewDiv" style={{ width: '100vw', height: '100vh' }}>
             </div>
         </div>
     );
