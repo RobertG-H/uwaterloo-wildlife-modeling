649fdea5470a7a9bb062c8e5ed40ec551e96308a
Robert Graham-Hu <robertgrahamhu@gmail.com>
2021-03-11 14:30:08 -0500

    Added functinoal opacity slider for base map

--- src/containers/OptionView/LayerView/LayerItem.tsx
+++ src/containers/OptionView/LayerView/LayerItem.tsx
@@ -4,11 +4,29 @@ import 'rc-slider/assets/index.css';
 import visibleIcon from '../../../assets//icons/general-icons/Toggle-Layer.png';
 import './layerViewStyle.css';
 
+import Layer from '@arcgis/core/layers/Layer';
+
 interface Props {
   title: string;
+  startingOpacity: number | undefined;
+  layer: Layer | undefined;
+  layer2: Layer | undefined;
 }
 
 const LayerItem = (props: Props) => {
+  const [opacity, setOpacity] = React.useState(props.startingOpacity);
+  const [visible, setVisible] = React.useState(true);
+
+  const setLayerOpacity = (newOpacity: number) => {
+    setOpacity(newOpacity);
+    if (props.layer) props.layer.opacity = newOpacity;
+    if (props.layer2) props.layer2.opacity = newOpacity;
+  };
+
+  const handleSlider = (sliderProps: any) => {
+    setLayerOpacity(sliderProps / 100);
+  };
+
   return (
     <div className='layer-item'>
       <div className='layer-item-title'>
@@ -18,7 +36,10 @@ const LayerItem = (props: Props) => {
       <div className='layer-item-opacity-title'>OPACITY</div>
       <div className='layer-item-slider'>
         <Slider
-          defaultValue={30}
+          defaultValue={props.startingOpacity ? props.startingOpacity * 100 : 100}
+          min={0}
+          max={100}
+          onChange={handleSlider}
           trackStyle={{ backgroundColor: '#a2b1d9', height: 4 }}
           handleStyle={{
             borderColor: '#a2b1d9',
--- src/containers/OptionView/LayerView/LayerViewContainer.tsx
+++ src/containers/OptionView/LayerView/LayerViewContainer.tsx
@@ -3,14 +3,25 @@ import LayerAccordian from './LayerAccordian';
 import LayerItem from './LayerItem';
 import LayerLegend from './LayerLegend';
 import LandCoverLegend from './LandCoverLegend';
+import { ArcContext } from '../../../context/ArcProvider';
 import './layerViewStyle.css';
 // interface Props {}
 
 const LayerViewContainer = (props: any) => {
+  const {
+    state: { authenticated, arcMap, arcMapView },
+    dispatch,
+  } = React.useContext(ArcContext);
+
   return (
     <div className='layer-view-container'>
       <LayerAccordian title='Default Layers'>
-        <LayerItem title='Base Map'></LayerItem>
+        <LayerItem
+          title='Base Map'
+          startingOpacity={arcMapView?.allLayerViews.getItemAt(0).layer.opacity}
+          layer={arcMapView?.allLayerViews.getItemAt(0).layer}
+          layer2={arcMapView?.allLayerViews.getItemAt(1).layer}
+        ></LayerItem>
       </LayerAccordian>
     </div>
   );
